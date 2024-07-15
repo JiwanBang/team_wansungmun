4a0440a7d6dd759e55c45376563377e9ec277e5c
kimgangmoon <rkdans2111@naver.com>
2024-07-16 03:09:37 +0900

    feat:incomplete_productUpload

--- wansungmoon_team/src/Components/Public/Body/Dropdown/index.tsx
+++ wansungmoon_team/src/Components/Public/Body/Dropdown/index.tsx
@@ -9,10 +9,10 @@ interface IProps {
 const Dropdown = ({ name = "", options, defaultStr }: IProps): JSX.Element => {
   return (
     <div className="relative p-1">
-      <div className="absolute w-5 h-5 right-2 top-2">
+      <div className="absolute w-5 h-5 right-3 top-3">
         <img src={arrow} className="w-full" />
       </div>
-      <select name={name} className="p-1 pr-5 border outline-none w-full appearance-none font-bold text-sm">
+      <select name={name} className="p-1 pr-6 border outline-none w-full appearance-none font-bold">
         <option value={""}>{defaultStr}</option>
         {options.map((item) => (
           <option value={item[0]}>{item[1]}</option>
