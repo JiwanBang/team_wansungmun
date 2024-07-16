636e5be60b87052160d3bd0f8e72138e9daea793
kimgangmoon <rkdans2111@naver.com>
2024-07-17 05:13:42 +0900

    feat:multer

--- wansungmoon_team/src/Components/Public/Body/InputBox/index.tsx
+++ wansungmoon_team/src/Components/Public/Body/InputBox/index.tsx
@@ -1,23 +1,23 @@
 import { ChangeEvent } from "react";
 import InputText from "./InputText";
 interface IProps {
-  sort: string;
+  title: string;
   value?: string;
   name?: string;
   placeholder?: string;
-  onInput?: (e: ChangeEvent<HTMLInputElement>) => {};
+  onInput?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const InputTextBox = ({
-  sort,
+  title,
   value,
   name = "",
   placeholder,
   onInput,
 }: IProps): JSX.Element => {
   return (
-    <div className="w-full">
-      <div className="p-1 pb-0 text-sm font-bold">{sort}</div>
+    <div>
+      <div className="p-1 pb-0 text-sm font-bold">{title}</div>
       <div>
         <InputText
           name={name}
