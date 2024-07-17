e7415dd4825de25d0800e3c0f3b675f326f328d6
kimgangmoon <rkdans2111@naver.com>
2024-07-18 02:42:05 +0900

    feat:map_work

--- wansungmoon_team/src/Components/Public/Body/Dropdown/index.tsx
+++ wansungmoon_team/src/Components/Public/Body/Dropdown/index.tsx
@@ -1,18 +1,24 @@
+import { ChangeEvent } from "react";
 import arrow from "../../../../imgs/Chevron-down.svg";
 
 interface IProps {
   options: [string, string][];
   name?: string;
   defaultStr: string;
+  onChange?: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
 
-const Dropdown = ({ name = "", options, defaultStr }: IProps): JSX.Element => {
+const Dropdown = ({ name = "", options, defaultStr, onChange }: IProps): JSX.Element => {
   return (
     <div className="relative p-1 w-full">
       <div className="absolute w-5 h-5 right-2 top-2">
         <img src={arrow} className="w-full" alt="" />
       </div>
-      <select name={name} className="p-1 pr-5 border outline-none w-full appearance-none font-bold text-sm">
+      <select
+        name={name}
+        className="p-1 pr-5 border outline-none w-full appearance-none font-bold text-sm"
+        onChange={onChange}
+      >
         <option value={""} key={0}>
           {defaultStr}
         </option>
