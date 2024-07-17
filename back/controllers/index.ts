9425c62851aa6880d3b2e21719846215788d93b6
kimgangmoon <kimgangmoon>
2024-07-17 15:16:27 +0900

    feat:incomplete_upload

--- back/controllers/index.ts
+++ back/controllers/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import test from "./test";
 import { Login } from "./login";
+import upload from "./upload";
 import sessions from "./session/index";
 import { Regist } from "./Regist";
 
@@ -9,5 +10,8 @@ router.use("/test", test);
 router.post("/login", Login);
 router.use("/regist", Regist);
 // router.use("/session", sessions);
+
+router.use("/test", test);
+router.use("/upload", upload);
 
 export default router;
