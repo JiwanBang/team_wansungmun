3b0ccb031077f688c3e36c89f14b781261262e9b
jiwanbang <jiwanbang@gmail.com>
2024-08-07 13:40:19 +0900

    fix: bug fix

--- wansungmoon_team/src/Components/Public/Body/KakaoMapLocationGetter/hooks/useKakaoLoader.ts
+++ wansungmoon_team/src/Components/Public/Body/KakaoMapLocationGetter/hooks/useKakaoLoader.ts
@@ -1,9 +1,8 @@
 import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk";
 
 export default function useKakaoLoader() {
-  console.log(`${process.env.REACT_APP_JSKEY}`);
   useKakaoLoaderOrigin({
-    appkey: `${process.env.REACT_APP_JSKEY}`, //본인 앱 키를 넣어주자.
+    appkey: `${process.env.REACT_APP_KAKAO_JSKEY}`, //본인 앱 키를 넣어주자.
     libraries: ["clusterer", "drawing", "services"],
   });
 }
